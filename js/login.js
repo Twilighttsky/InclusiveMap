2d21bf4468a4d108ad9c0e92dd979473be5fbe23
Jikkan <69170077+Twilighttsky@users.noreply.github.com>
2024-12-01 14:23:24 -0300

    updade login
    foram adicionados icones e o link para página de cadastro, mudei a curvatura do plano de fundo, add transição nos inputs, foi adicionado a parte de JS que faz com que a senha seja revelada ao clicar no icone de olho e o ajuste dos elementos para ficarem centralizados e de acordo com as telas

--- js/login.js
+++ js/login.js
@@ -1,2 +1,12 @@
-const formulario = document.getElementById(formulario);
-formulario.reset();
+function changePasswordVisibility() {
+    let inputPass = document.getElementById('senha_log')
+    let btnShowPassword = document.getElementById('btn-senha')
+
+    if (inputPass.type === 'password') {
+    inputPass.setAttribute('type','text')
+    btnShowPassword.classList.replace('fa-eye', 'fa-eye-slash')
+    } else {
+        inputPass.setAttribute('type','password')
+        btnShowPassword.classList.replace('fa-eye-slash', 'fa-eye')
+    }
+}
